9ab2c62e9963323bc0a60cf8ee9cdbf39ae37d53
Anthony Chiu <anthony9086@hotmail.com>
2021-12-28 16:35:44 +0800

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Event.destroy_all
+User.destroy_all
+Signup.destroy_all
+
+p "Destroyed all data"
+
+user = User.create!(
+    name: Faker::TvShows::TheExpanse.character
+    10.times do 
+        event = Event.create(
+            name: Faker::TvShows::TheExpanse.ship,
+            event_summary: Faker::TvShows::TheExpanse.quote,
+            location: Faker::TvShows::TheExpanse.location,
+            event_date: Faker::Date.between(from: '2021-01-01', to: '2022-12-31'),
+            start_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now,
+            end_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now,
+            seat_capacity: rand(0..20),
+            event_image: "PlaceholderImageURL"
+        )
+            rand(0..20).times do
+                Signup.create(
+                    phone_number: Faker::PhoneNumber.phone_number,
+                )
+            end
+        event.save!
+    end
+)
+
