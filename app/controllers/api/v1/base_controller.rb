09c8ebef84a39aae0bc2c66fba333d26129b5e59
Anthony Chiu <anthony9086@hotmail.com>
2022-01-15 14:26:04 +0800

    used headers

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,9 +1,28 @@
 class Api::V1::BaseController < ApplicationController
   rescue_from StandardError,                with: :internal_server_error
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  # before_action :authenticate_user!
   skip_before_action :verify_authenticity_token
   
   private
+
+  # def authenticate_user!
+  #   current_user || invalid_authentication
+  # end
+
+  # def invaild_authentication
+  #   render json: { error: { message: "USER AUTHENTICATION FAILED!", code: nil } }, status: :unauthorized
+  # end
+
+  def current_user
+    p "-------------THIS IS CURRENT USER-------------"
+    p "-------------USER HEADERS-------------"
+    # p request.headers
+    p "-------------USER HEADERS-------------"
+    user_id = request.headers['X-USER-ID']&.split(' ')&.last.to_i
+    @current_user ||= User.find_by(id: user_id) if user_id
+    p "-------------THIS IS CURRENT USER-------------"
+  end
 
   def not_found(exception)
     render json: { error: exception.message }, status: :not_found
@@ -13,8 +32,10 @@ class Api::V1::BaseController < ApplicationController
     if Rails.env.development?
       response = { type: exception.class.to_s, error: exception.message }
     else
-      response = { error: "Internal Server Error" }
+      response = { error: "INTERNAL SERVER ERROR" }
     end
     render json: response, status: :internal_server_error
   end
+
+
 end
